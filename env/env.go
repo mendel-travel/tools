3fd82beb22ffeef07497798b00f71f4bac892087
Matias Sueiro <matias.sueiro@basset.la>
2017-10-04 15:26:30 -0300

    fix

--- env/env.go
+++ env/env.go
@@ -1,28 +1,17 @@
 package env
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 
 	"gopkg.in/yaml.v2"
 )
 
-// ENV : contains the setted environment value
-var ENV string
-
-func createFlagsForBuild() {
-	flag.StringVar(&ENV, "e", "development", "Specify environment")
-	flag.Parse()
-}
-
 // LoadProperties : function that receives a forlder name where the config files are located and a interface to map the file.
 // We decide to use a struct to manage de properties because is easy to access values after in the code.
-func LoadProperties(f string, p interface{}) {
+func LoadProperties(f string, e string, p interface{}) {
 
-	createFlagsForBuild()
-
-	file, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/properties.yaml", f, ENV))
+	file, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/properties.yaml", f, e))
 
 	if err != nil {
 		panic("I can't find the env properties file. Please put the file and compile the project again!")
