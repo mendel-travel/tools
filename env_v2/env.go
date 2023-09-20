fa661714c46312610de93c7648c21dd0a51f816f
srodrmendz <sebastian.rodriguez@basset.la>
2023-09-20 09:15:59 -0400

    refactor: env v2 file

--- env_v2/env.go
+++ env_v2/env.go
@@ -1,35 +1,27 @@
 package env
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
 
-// e var : contains the setted environment value
-var e string
-
-func createFlagsForBuild(v string) {
-	flag.StringVar(&e, v, "development", "Specify environment")
-	flag.Parse()
-}
-
 // LoadProperties : function that receives a forlder name where the config files are located and a interface to map the file.
 // We decide to use a struct to manage de properties because is easy to access values after in the code.
 func LoadProperties(v string, f string, p interface{}) {
-
-	createFlagsForBuild(v)
-
-	file, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/properties.yaml", f, e))
-
-	if err != nil {
-		panic("I can't find the env properties file. Please put the file and compile the project again!")
-	} else {
-		if err := yaml.Unmarshal(file, p); err != nil {
-			panic("There is an error on .yaml properties. Please check the file and try again!")
-		}
+	env := os.Getenv(v)
+	if env == "" {
+		env = "development"
 	}
 
+	file, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/properties.yaml", f, env))
+	if err != nil {
+		panic("I can't find the env properties file. Please put the file and compile the project again!")
+	}
+
+	if err := yaml.Unmarshal(file, p); err != nil {
+		panic("There is an error on .yaml properties. Please check the file and try again!")
+	}
 }
